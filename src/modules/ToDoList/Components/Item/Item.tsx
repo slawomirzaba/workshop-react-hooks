83ad11a3ed8785418b8abe697cc2700a8e19239c
mmiszuta <mmiszuta@rst.com.pl>
2019-02-10 10:46:53 +0100

    added componentWillUnmount hook version inside item

--- src/modules/ToDoList/Components/Item/Item.tsx
+++ src/modules/ToDoList/Components/Item/Item.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, FunctionComponent } from 'react';
+import React, { ReactElement, FunctionComponent, useEffect } from 'react';
 import { FinishedItemActions } from '../FinishedItemActions';
 import { ToDoItemActions } from '../ToDoItemActions';
 import { ItemI } from '../../interfaces/ItemI';
@@ -14,21 +14,32 @@ export const Item: FunctionComponent<PropsI> = ({
     isImportant,
     removeItem,
     id,
-}: PropsI): ReactElement<PropsI> => (
-    <div className={`items-column__item ${isImportant ? 'items-column__item--important' : ''}`}>
-        <div className="items-column__item-title">
-            {isImportant && (
-                <span className="items__title-important-icon" title="Important item">
-                    <i className="fas fa-exclamation-triangle" />
-                </span>
-            )}{' '}
-            {title}
-            <div className="items-column__item-description">{description}</div>
+}: PropsI): ReactElement<PropsI> => {
+    useEffect(() => {
+        return () => {
+            alert('Item has been deleted');
+        }
+    }, []);
+    return (
+        <div className={`items-column__item ${isImportant ? 'items-column__item--important' : ''}`}>
+            <div className="items-column__item-title">
+                {isImportant && (
+                    <span className="items__title-important-icon" title="Important item">
+                        <i className="fas fa-exclamation-triangle" />
+                    </span>
+                )}{' '}
+                {title}
+                <div className="items-column__item-description">{description}</div>
+            </div>
+            {isFinished ? (
+                <FinishedItemActions itemId={id} removeItem={removeItem} />
+            ) : (
+                <ToDoItemActions
+                    isImportantItem={isImportant}
+                    itemId={id}
+                    removeItem={removeItem}
+                />
+            )}
         </div>
-        {isFinished ? (
-            <FinishedItemActions itemId={id} removeItem={removeItem} />
-        ) : (
-            <ToDoItemActions isImportantItem={isImportant} itemId={id} removeItem={removeItem} />
-        )}
-    </div>
-);
+    );
+};
