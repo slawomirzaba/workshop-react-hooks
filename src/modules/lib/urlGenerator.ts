cb78eaa6ebdfbfe8198c0182d2978de2e438be24
szaba <slawomir.zaba@rst.com.pl>
2019-02-24 11:29:32 +0100

    adjust common changes to all branches

--- src/modules/lib/urlGenerator.ts
+++ src/modules/lib/urlGenerator.ts
@@ -1,4 +1,6 @@
-const TO_DO_ITEMS_URL = 'https://react-hooks-to-do-list.firebaseio.com/items';
+import config from '../../config.json';
+
+const TO_DO_ITEMS_URL = config.TO_DO_ITEMS_URL;
 
 export const getFetchAndPostItemsUrl = () => `${TO_DO_ITEMS_URL}.json`;
 
