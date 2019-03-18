5fb2514707c41331e8da3bdcb9b91e86b984b517
szaba <slawomir.zaba@rst.com.pl>
2019-03-18 09:34:48 +0100

    impoving reducer

--- src/modules/ToDoList/Container/Container.tsx
+++ src/modules/ToDoList/Container/Container.tsx
@@ -1,61 +1,22 @@
-import React, {
-    FunctionComponent,
-    ReactElement,
-    useState,
-    useEffect,
-    useMemo,
-    useReducer,
-} from 'react';
+import React, { FunctionComponent, ReactElement, useEffect, useMemo, useReducer } from 'react';
 import { ItemForm } from '../Components/ItemForm';
 import { ItemsLists } from '../Components/ItemsLists';
 import { ItemI, DataBaseItemI } from '../interfaces/ItemI';
 import { fetchItems, saveItem, deleteItem, updateItem } from '../services';
-import { calculateProgressValue } from '../lib';
+import { itemsReducer } from './itemsReducer';
 
 const SET_ITEMS = 'SET_ITEMS';
 const ADD_ITEM = 'ADD_ITEM';
 const REMOVE_ITEM = 'REMOVE_ITEM';
 const UPDATE_ITEM = 'UPDATE_ITEM';
 
-interface ActionI {
-    type: string;
-    items?: ItemI[];
-    item?: ItemI;
-    itemId?: string;
-    updatedProperties?: Partial<ItemI>;
-}
-
-const itemsReducer = (items: ItemI[] = [], action: ActionI): ItemI[] => {
-    switch (action.type) {
-        case 'SET_ITEMS': {
-            return action.items || [];
-        }
-        case 'ADD_ITEM': {
-            return action.item ? [...items, action.item] : items;
-        }
-        case 'REMOVE_ITEM': {
-            return items.filter((item: ItemI) => item.id !== action.itemId);
-        }
-        case 'UPDATE_ITEM': {
-            return items.map((item: ItemI) => {
-                if (item.id !== action.itemId) return item;
-                return {
-                    ...item,
-                    ...action.updatedProperties,
-                };
-            });
-        }
-        default: {
-            return items;
-        }
-    }
-};
-
 export const Container: FunctionComponent = (): ReactElement<{}> => {
-    const [items, dispatchItems] = useReducer(itemsReducer, []);
-    const [toDoItems, setToDoItems] = useState<ItemI[]>([]);
-    const [finishedItems, setFinishedItems] = useState<ItemI[]>([]);
-    const [finishedItemsPercentage, setFinishedItemsPercentage] = useState(0);
+    const [itemsState, dispatchItems] = useReducer(itemsReducer, {
+        items: [],
+        toDoItems: [],
+        finishedItems: [],
+        finishedItemsPercentage: 0,
+    });
 
     useEffect(() => {
         fetchItems().then((newItems: ItemI[]) => {
@@ -65,24 +26,6 @@ export const Container: FunctionComponent = (): ReactElement<{}> => {
             });
         });
     }, []);
-
-    useEffect(() => {
-        const newToDoItems: ItemI[] = [];
-        const newFinishedItems: ItemI[] = [];
-
-        items.forEach((item: ItemI) => {
-            if (item.isFinished) newFinishedItems.push(item);
-            else newToDoItems.push(item);
-        });
-        const newFinishedItemsPercentage = calculateProgressValue(
-            newToDoItems.length,
-            newFinishedItems.length,
-        );
-
-        setToDoItems(newToDoItems);
-        setFinishedItems(newFinishedItems);
-        setFinishedItemsPercentage(newFinishedItemsPercentage);
-    }, [items]);
 
     const addItem = (title: string, description: string, isImportant: boolean): void => {
         const newItem: DataBaseItemI = {
@@ -122,7 +65,7 @@ export const Container: FunctionComponent = (): ReactElement<{}> => {
     };
 
     const toggleIsImportantItem = (itemId: string): void => {
-        const currentItem = items.find(item => item.id === itemId);
+        const currentItem = itemsState.items.find(item => item.id === itemId);
         if (!currentItem) return;
 
         const newIsImportantState = !currentItem.isImportant;
@@ -148,9 +91,9 @@ export const Container: FunctionComponent = (): ReactElement<{}> => {
                     [],
                 )}
                 <ItemsLists
-                    toDoItems={toDoItems}
-                    finishedItems={finishedItems}
-                    finishedItemsPercentage={finishedItemsPercentage}
+                    toDoItems={itemsState.toDoItems}
+                    finishedItems={itemsState.finishedItems}
+                    finishedItemsPercentage={itemsState.finishedItemsPercentage}
                     removeItem={removeItem}
                     setIsFinishedItem={setIsFinishedItem}
                     toggleIsImportantItem={toggleIsImportantItem}
